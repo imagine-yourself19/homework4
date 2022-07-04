795254131cc0c7e27fae3fc652233de9db8c92ab
Lena <imagine.youeself19@gmail.com>
2022-07-04 16:02:46 +0300

    изменено решение задачи 25

--- задача25/Program.cs
+++ задача25/Program.cs
@@ -1,6 +1,14 @@
-﻿Console.WriteLine("Введите числа A и B для возведения числа A в степень B");
-double A = Convert.ToInt32(Console.ReadLine());
-double B = Convert.ToInt32(Console.ReadLine());
-double result;
-result = Math.Pow(A, B);
-Console.WriteLine($"Результат возведения числа {A} в степень {B} равен {result}");
+﻿Console.WriteLine("Введите числа a и b для возведения числа a в степень b");
+long a = Convert.ToInt32(Console.ReadLine());
+long b = Convert.ToInt32(Console.ReadLine());
+long power(long a, long b) {
+    long result = 1;
+
+    for(long i = 0; i < b; i++) {
+        result *= a;
+    }
+
+    return result;
+}
+long res = power(a, b);
+Console.WriteLine($"Число {a} возведенное в степень {b} равно {res}");
