a994bad2d997b4c70ed87dd5241a1d41a99d1d69
Lena <imagine.youeself19@gmail.com>
2022-07-04 15:05:58 +0300

    добавлена задача 27

---
+++ задача27/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число для получения суммы его цифр");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = n; i > 0; i = i / 10)
+{
+sum = sum + i % 10;
+}
+Console.WriteLine(sum);
