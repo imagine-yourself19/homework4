82402abf934a1043ecd1a3e58933c88cd22aada6
Lena <imagine.youeself19@gmail.com>
2022-07-05 14:00:40 +0300

    добавлена идея решения задачи29

---
+++ задача29/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    collection[index] = Convert.ToInt32(Console.ReadLine());
+    while (index < length)
+    {
+       collection[index] = // new Random().Next(); по идее где-то в этом месте нужно заставить систему считать данные, введенные пользователем, вместо Random
+       index++; 
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+      Console.WriteLine(coll[position]);
+      position++;  
+    }
+}
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
