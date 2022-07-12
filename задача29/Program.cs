5239fb0959e2b53487492d1da60ab3524ae46fda
Lena <imagine.youeself19@gmail.com>
2022-07-12 11:21:23 +0300

    добавлено рабочее решение задачи 29

--- задача29/Program.cs
+++ задача29/Program.cs
@@ -1,26 +1,9 @@
-﻿void FillArray(int[] collection)
+﻿Console.WriteLine("Введи кол-во элементов в массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
 {
-    int length = collection.Length;
-    int index = 0;
-    collection[index] = Convert.ToInt32(Console.ReadLine());
-    while (index < length)
-    {
-       collection[index] = // new Random().Next(); по идее где-то в этом месте нужно заставить систему считать данные, введенные пользователем, вместо Random
-       index++; 
-    }
+Console.WriteLine("Введи число: ");
+array[i] = Convert.ToInt32(Console.ReadLine());
 }
-
-void PrintArray(int[] coll)
-{
-    int count = coll.Length;
-    int position = 0;
-    while (position < count)
-    {
-      Console.WriteLine(coll[position]);
-      position++;  
-    }
-}
-int[] array = new int[8];
-
-FillArray(array);
-PrintArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
